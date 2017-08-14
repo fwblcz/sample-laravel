c795712f909f0a75a528d46869c5566ae21549fd
fuwenbin <1964855183@qq.com>
2017-08-14 03:47:08 +0000

    say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-sample
                 </div>
 
                 <div class="links">
